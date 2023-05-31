f482d67ae997ceec3ebc49cb2e8fb436527f5a86
Leo Dion <leogdion@brightdigit.com>
2023-05-31 13:58:41 -0400

    completed setup

---
+++ Tests/NPMPublishPluginTests/NPMPublishPluginTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import NPMPublishPlugin
+
+final class NPMPublishPluginTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
