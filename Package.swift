2d467b2f9c5e8c036dcb888b82a53ec6f283aa37
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-06-01 17:24:32 +0300

    Migrate Sources of NPMPublishPlugin to this package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,35 @@
 import PackageDescription
 
 let package = Package(
-  name: "NPMPublishPlugin",
-  products: [
-    .library(
-      name: "NPMPublishPlugin",
-      targets: ["NPMPublishPlugin"]
-    )
-  ],
-  targets: [
-    .target(
-      name: "NPMPublishPlugin"
-    ),
-    .testTarget(
-      name: "NPMPublishPluginTests",
-      dependencies: ["NPMPublishPlugin"]
-    )
-  ]
+    name: "NPMPublishPlugin",
+    platforms: [.macOS(.v12)],
+    products: [
+        .library(
+            name: "NPMPublishPlugin",
+            targets: ["NPMPublishPlugin"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/johnsundell/publish.git",
+            from: "0.9.0"
+        ),
+        .package(
+            url: "https://github.com/johnsundell/shellout.git",
+            from: "2.3.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "NPMPublishPlugin",
+            dependencies: [
+                .product(name: "Publish", package: "publish"),
+                .product(name: "ShellOut", package: "shellout")
+            ]
+        ),
+        .testTarget(
+            name: "NPMPublishPluginTests",
+            dependencies: ["NPMPublishPlugin"]
+        )
+    ]
 )
